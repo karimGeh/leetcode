528b595552a66d29fb27e1691d2f6f8d8c6f3109
Karim G <karim2jihad@gmail.com>
2022-04-27 23:05:29 +0000

    27. Remove Element third solution

---
+++ easy/27. Remove Element/solution3.py
@@ -0,0 +1,12 @@
+import typing
+
+
+class Solution:
+  def removeElement(self, nums: typing.List[int], val: int) -> int:
+    i = 0
+    for j in range(len(nums)):
+      if nums[j] != val:
+        nums[i] = nums[j]
+        i += 1
+
+    return i
